ce17ad7c5391b192d35f0ab2191327edcd43072f
caleb <musyokacaleb5@gmail.com>
2025-03-16 21:35:28 +0300

    completion of this lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def __str__(self):
+        return f"'{self.title}', {self.page_count} pages"
+
+# Example usage:
+# book = Book("1984", 328)
+# print(book)
+# book.turn_page()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self.color = color
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    def __str__(self):
+        return f"{self.color} {self.brand} shoe, size {self.size}"
+
+# Example usage:
+# shoe = Shoe("Nike", 42, "black")
+# print(shoe)
+# shoe.cobble()
